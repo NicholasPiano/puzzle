6d858923fa0f24e182df15ca7fa7de9171bfeefe
NicholasPiano <nicholas.d.piano@gmail.com>
2015-01-26 22:33:30 +0000

    moving urls

--- wsgi.py
+++ wsgi.py
@@ -18,7 +18,7 @@ framework.
 
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'puzzle.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'woot.settings.development')
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
@@ -29,4 +29,3 @@ application = get_wsgi_application()
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
-
