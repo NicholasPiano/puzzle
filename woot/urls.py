9d23c90783c5b7a7cd96594e8856f972e33f5af6
NicholasPiano <nicholas.d.piano@gmail.com>
2015-01-26 22:37:13 +0000

    url imports

--- woot/urls.py
+++ woot/urls.py
@@ -1,6 +1,6 @@
 #puzzle.urls
 
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView, RedirectView
 
 from django.contrib import admin
