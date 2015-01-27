af40b50d4ce38a4f75356f1f93d881720731799b
NicholasPiano <nicholas.d.piano@gmail.com>
2015-01-27 16:12:36 +0000

    First run.
    Database configured

--- woot/apps/cell/management/commands/input.py
+++ woot/apps/cell/management/commands/input.py
@@ -40,6 +40,7 @@ class Command(BaseCommand):
 
     #for each filename, generate dictionary of parameters
     for image_file_name in image_file_list:
+      self.stout.write(image_file_name + '... ', ending='')
       match = re.match(img_settings.img_template, image_file_name)
       #need path, experiment_name, image_type, timepoint, level, and extension to make image object
       #1. check if experiment exists
@@ -53,3 +54,6 @@ class Command(BaseCommand):
         image.level = int(match.group('level'))
         image.save()
         experiment.save()
+        self.stout.write('created.', ending='\n')
+      else:
+        self.stout.write('already exists.', ending='\n')
