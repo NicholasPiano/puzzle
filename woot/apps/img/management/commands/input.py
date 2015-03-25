bfab521c04cbbaa6bee40fef6512018f001e3e62
NicholasPiano <nicholas.d.piano@gmail.com>
2015-03-25 06:42:50 +0000

    modding for partial input

--- woot/apps/img/management/commands/input.py
+++ woot/apps/img/management/commands/input.py
@@ -41,37 +41,38 @@ class Command(BaseCommand):
 
     # check if experiment exists
     experiment, exp_created = Experiment.objects.get_or_create(name=experiment_name)
-    if exp_created:
-      experiment.make_paths(base_path)
-      experiment.get_metadata()
-
-    # list directory filtered by allow extension
-    file_list = [file_name for file_name in os.listdir(experiment.img_path) if os.path.splitext(file_name)[1] in allowed_img_extensions]
-
-    # make paths and series
-    for i, file_name in enumerate(file_list):
-
-      # get template
-      template = experiment.match_template(file_name)
-      path, created = template.get_or_create_path(experiment.img_path, file_name)
-
-      print('%s... %s' % (path, 'created.' if created else 'already exists.'))
+    # if exp_created:
+    #   experiment.make_paths(base_path)
+    #   experiment.get_metadata()
+    #
+    # # list directory filtered by allow extension
+    # file_list = [file_name for file_name in os.listdir(experiment.img_path) if os.path.splitext(file_name)[1] in allowed_img_extensions]
+    #
+    # # make paths and series
+    # for i, file_name in enumerate(file_list):
+    #
+    #   # get template
+    #   template = experiment.match_template(file_name)
+    #   path, created = template.get_or_create_path(experiment.img_path, file_name)
+    #
+    #   print('%s... %s' % (path, 'created.' if created else 'already exists.'))
 
     # make composites and masks
 
     # set extent for each series
-    for series in Series.objects.all():
-      if series.rs==-1:
-        # rows and columns from image
-        (rs,cs) = series.paths.get(channel=series.experiment.channels.all()[0], t=0, z=0).load().shape
-        series.rs = rs
-        series.cs = cs
+    for series in experiment.series.all():
+      if experiment.allow_series(series.name):
+        if series.rs==-1:
+          # rows and columns from image
+          (rs,cs) = series.paths.get(channel=series.experiment.channels.all()[0], t=0, z=0).load().shape
+          series.rs = rs
+          series.cs = cs
 
-        # z and t from counts
-        series.zs = series.paths.filter(channel=series.experiment.channels.all()[0], t=0).count()
-        series.ts = series.paths.filter(channel=series.experiment.channels.all()[0], z=0).count()
+          # z and t from counts
+          series.zs = series.paths.filter(channel=series.experiment.channels.all()[0], t=0).count()
+          series.ts = series.paths.filter(channel=series.experiment.channels.all()[0], z=0).count()
 
-        series.save()
+          series.save()
 
-      if series.composites.count()==0:
-        series.compose()
+        if series.composites.count()==0:
+          series.compose()
