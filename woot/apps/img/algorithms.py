78bf011574960f839a8b7660703251975f395b30
NicholasPiano <nicholas.d.piano@gmail.com>
2015-04-02 12:03:05 +0100

    removing mod_primary

--- woot/apps/img/algorithms.py
+++ woot/apps/img/algorithms.py
@@ -51,43 +51,6 @@ def mod_pmod(composite, mod_id, algorithm):
 
 mod_pmod.description = 'Scale portions of the brightfield using the gfp density.'
 
-def mod_primary(composite, mod_id, algorithm):
-  # paths
-  template = composite.templates.get(name='source')
-  url = os.path.join(composite.experiment.composite_path, template.rv)
-
-  # channel
-  channel = composite.channels.create(name='1-%s-%s-%s' % (composite.id_token, 'primary', mod_id))
-
-  # get all cell markers
-  for t in range(composite.series.ts):
-    print(t)
-
-    markers = composite.series.markers.filter(t=t)
-
-    # black field in which to place markers
-    b = np.zeros((composite.series.rs, composite.series.cs), dtype='uint8')
-
-    for marker in markers:
-      b[marker.r-1:marker.r+1, marker.c-1:marker.c+1] = 255
-
-    # duplicate over z
-    out = np.dstack([b]*composite.series.zs)
-
-    # 3. output
-    gon = composite.gons.create(experiment=composite.experiment, series=composite.series, channel=channel)
-    gon.set_origin(0, 0, 0, t)
-    gon.set_extent(composite.series.rs, composite.series.cs, composite.series.zs)
-
-    gon.array = out
-
-    gon.save_paths(url, template)
-    gon.split()
-
-    gon.save()
-
-mod_primary.description = 'Create channel containing track points to set as primary in Cell Profiler.'
-
 def mod_reduced(composite, mod_id, algorithm):
   # paths
   template = composite.templates.get(name='source')
