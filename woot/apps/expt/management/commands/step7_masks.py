4b782af57e9ae2ed043ac0cfa66febb427614fd8
NicholasPiano <nicholas.d.piano@gmail.com>
2015-04-13 18:12:15 +0100

    only process series if it's in the name

--- woot/apps/expt/management/commands/step7_masks.py
+++ woot/apps/expt/management/commands/step7_masks.py
@@ -29,52 +29,51 @@ class Command(BaseCommand):
       template = series.experiment.templates.get(name='cp')
 
       for file_name in file_list:
-        # print('processing mask path %s... ' % file_name, end='\r')
+        print('processing mask path %s... ' % file_name, end='\r')
         file_dict = template.dict(file_name)
 
-        # 1. get current composite, channel
-        print(file_dict['composite_id'])
-        c = series.composites.get()
-        print(c.id_token)
-        composite = series.composites.get(id_token=file_dict['composite_id'])
-        channel = composite.channels.get(name=file_dict['channel'])
-        cp_template = composite.templates.get(name='cp')
-        mask_template = composite.templates.get(name='mask')
+        if series.name==file_dict['series']:
 
-        # 2. get or create channel
-        mask_channel, mask_channel_created = composite.channels.get_or_create(name=file_dict['secondary_channel'])
+          # 1. get current composite, channel
+          composite = series.composites.get(id_token=file_dict['composite_id'])
+          channel = composite.channels.get(name=file_dict['channel'])
+          cp_template = composite.templates.get(name='cp')
+          mask_template = composite.templates.get(name='mask')
 
-        # 3. get or create gon only if path does not yet exist
-        path_url = os.path.join(series.experiment.mask_path, file_name)
-        if mask_channel.paths.filter(url=path_url).count()==0:
-          t = int(file_dict['t'])
-          z = int(file_dict['z'])
+          # 2. get or create channel
+          mask_channel, mask_channel_created = composite.channels.get_or_create(name=file_dict['secondary_channel'])
 
-          gon = series.gons.create(experiment=series.experiment, composite=composite, channel=mask_channel)
-          gon.set_origin(0,0,z,t)
-          gon.set_extent(series.rs, series.cs, 1)
+          # 3. get or create gon only if path does not yet exist
+          path_url = os.path.join(series.experiment.mask_path, file_name)
+          if mask_channel.paths.filter(url=path_url).count()==0:
+            t = int(file_dict['t'])
+            z = int(file_dict['z'])
 
-          gon.paths.create(composite=composite, channel=mask_channel, template=cp_template, url=path_url, file_name=file_name, t=t, z=z)
+            gon = series.gons.create(experiment=series.experiment, composite=composite, channel=mask_channel)
+            gon.set_origin(0,0,z,t)
+            gon.set_extent(series.rs, series.cs, 1)
 
-          gon.save()
+            gon.paths.create(composite=composite, channel=mask_channel, template=cp_template, url=path_url, file_name=file_name, t=t, z=z)
 
-          # open file and get split into masks
-          mask_array = gon.load()
-          for i, unique_id in enumerate([u for u in np.unique(mask_array) if u>0]):
-            print('processing mask path %s... %d masks' % (file_name, (i+1)) , end='\r')
+            gon.save()
 
-            # make mask
-            mask = gon.masks.create(composite=composite, channel=mask_channel, mask_id=unique_id)
+            # open file and get split into masks
+            mask_array = gon.load()
+            for i, unique_id in enumerate([u for u in np.unique(mask_array) if u>0]):
+              print('processing mask path %s... %d masks' % (file_name, (i+1)) , end='\r')
 
-            # cut
-            unique_image = np.zeros(mask_array.shape)
-            unique_image[mask_array==unique_id] = 1
-            cut, (r,c,rs,cs) = cut_to_black(unique_image)
+              # make mask
+              mask = gon.masks.create(composite=composite, channel=mask_channel, mask_id=unique_id)
 
-            mask.r = r
-            mask.c = c
-            mask.rs = rs
-            mask.cs = cs
-            mask.save()
+              # cut
+              unique_image = np.zeros(mask_array.shape)
+              unique_image[mask_array==unique_id] = 1
+              cut, (r,c,rs,cs) = cut_to_black(unique_image)
 
-          print('processing mask path %s... %d masks... done.' % (file_name, (i+1)) , end='\n')
+              mask.r = r
+              mask.c = c
+              mask.rs = rs
+              mask.cs = cs
+              mask.save()
+
+            print('processing mask path %s... %d masks... done.' % (file_name, (i+1)) , end='\n')
