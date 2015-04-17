da9396adb0f2836dff249769ec83d9ddff66db08
NicholasPiano <nicholas.d.piano@gmail.com>
2015-04-17 10:52:03 +0100

    print

--- woot/apps/img/algorithms.py
+++ woot/apps/img/algorithms.py
@@ -328,6 +328,7 @@ def mod_region_img(composite, mod_id, algorithm):
 
     # get middle z level
     middle_z = int(composite.series.zs / 2.0)
+    print(middle_z)
 
     # get single bf plane at z
     bf = bf_set.get(t=t, z=middle_z).load()
