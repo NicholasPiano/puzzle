b8c3541abfe6f0458b64cf472eb10aedfe54746f
NicholasPiano <nicholas.d.piano@gmail.com>
2015-04-18 16:47:33 +0100

    updating tracking

--- woot/apps/expt/management/commands/step02_tracking.py
+++ woot/apps/expt/management/commands/step02_tracking.py
@@ -3,21 +3,49 @@ from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
 #local
-from apps.img.models import Series
-from apps.img.settings import *
+from apps.expt.util import *
+from apps.img import algorithms
+from apps.img.models import Composite
 
 #util
 import os
-import re
-import numpy as np
-from scipy.misc import imsave
-import matplotlib.pyplot as plt
+from optparse import make_option
 
 ### Command
 class Command(BaseCommand):
+
+  option_list = BaseCommand.option_list + (
+
+    make_option('--expt', # path of images unique to this experiment.
+      action='store',
+      dest='expt',
+      default='050714-test',
+      help='Name of experiment to modify'
+    ),
+
+    make_option('--series', # defines base search path. All images are in a subdirectory of this directory.
+      action='store',
+      dest='series',
+      default='13',
+      help='Name of series to modify'
+    ),
+
+    make_option('--composite', # path of images unique to this experiment.
+      action='store',
+      dest='composite',
+      default='0',
+      help='Composite to add to'
+    ),
+
+  )
 
   args = ''
   help = ''
 
   def handle(self, *args, **options):
-    pass
+    # 1. get composite
+    composite = Composite.objects.get(experiment__name=options['expt'], series__name=options['series'])
+
+    # 2. Create mod
+    mod = composite.mods.create(id_token=generate_id_token('img', 'Mod'), algorithm='mod_step2_tracking')
+    mod.run()
