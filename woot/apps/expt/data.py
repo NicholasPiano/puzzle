38aac48032d242abbb6fcbb40551d164099ed84a
NicholasPiano <nicholas.d.piano@gmail.com>
2015-04-20 18:43:43 +0100

    updating data for more series
    More updating is needed to get regions correct

--- woot/apps/expt/data.py
+++ woot/apps/expt/data.py
@@ -31,17 +31,14 @@ class _Region():
 ### Data
 experiments = (
   _Experiment(name='050714', rmop=0.5369, cmop=0.5369, zmop=1.482, tpf=10.7003),
-  _Experiment(name='050714-test', rmop=0.5369, cmop=0.5369, zmop=1.482, tpf=10.7003),
   _Experiment(name='190714', rmop=0.501, cmop=0.5015, zmop=1.482, tpf=9.7408),
   _Experiment(name='260714', rmop=0.5696074, cmop=0.5701647, zmop=1.482, tpf=7.6807),
+  _Experiment(name='280614', rmop=0.7941950, cmop=0.7934188, zmop=1.482, tpf=10.665),
 )
 
 series = (
   # 050714
   _Series(experiment='050714', name='13'),
-
-  # 050714-test
-  _Series(experiment='050714-test', name='13'),
 
   # 190714
   _Series(experiment='190714', name='12'),
@@ -51,26 +48,54 @@ series = (
   _Series(experiment='260714', name='13'),
   _Series(experiment='260714', name='14'),
   _Series(experiment='260714', name='15'),
+
+  # 280614
+  _Series(experiment='280614', name='7'),
 )
 
 regions = (
-  # 050714
+  # 050714 - series 13
   _Region(experiment='050714', series='13', name='medium', description='Bottom of the environment in the medium', index=1, vertical_sort_index=4),
   _Region(experiment='050714', series='13', name='barrier-edge', description='Within one cell diameter of the barrier', index=2, vertical_sort_index=3),
   _Region(experiment='050714', series='13', name='barrier', description='Within the barrier', index=3, vertical_sort_index=2),
   _Region(experiment='050714', series='13', name='gel', description='Through the barrier in the gel region', index=4, vertical_sort_index=1),
 
-  # 050714-test
-  _Region(experiment='050714-test', series='13', name='medium', description='Bottom of the environment in the medium', index=1, vertical_sort_index=4),
-  _Region(experiment='050714-test', series='13', name='barrier-edge', description='Within one cell diameter of the barrier', index=2, vertical_sort_index=3),
-  _Region(experiment='050714-test', series='13', name='barrier', description='Within the barrier', index=3, vertical_sort_index=2),
-  _Region(experiment='050714-test', series='13', name='gel', description='Through the barrier in the gel region', index=4, vertical_sort_index=1),
+  # 190714 - series 12
+  _Region(experiment='190714', series='12', name='medium', description='Bottom of the environment in the medium', index=1, vertical_sort_index=4),
+  _Region(experiment='190714', series='12', name='barrier-edge', description='Within one cell diameter of the barrier', index=2, vertical_sort_index=3),
+  _Region(experiment='190714', series='12', name='barrier', description='Within the barrier', index=3, vertical_sort_index=2),
+  _Region(experiment='190714', series='12', name='gel', description='Through the barrier in the gel region', index=4, vertical_sort_index=1),
 
-  # 190714
-  # _Region(experiment='', series='', name='', description='', index=, vertical_sort_index=)
-  # _Region(experiment='', series='', name='', description='', index=, vertical_sort_index=)
-  # _Region(experiment='', series='', name='', description='', index=, vertical_sort_index=)
-  # _Region(experiment='', series='', name='', description='', index=, vertical_sort_index=)
+  # 260714 - series 12
+  _Region(experiment='260714', series='12', name='medium', description='Bottom of the environment in the medium', index=1, vertical_sort_index=4),
+  _Region(experiment='260714', series='12', name='barrier-edge', description='Within one cell diameter of the barrier', index=2, vertical_sort_index=3),
+  _Region(experiment='260714', series='12', name='barrier', description='Within the barrier', index=3, vertical_sort_index=2),
+  _Region(experiment='260714', series='12', name='gel', description='Through the barrier in the gel region', index=4, vertical_sort_index=1),
+
+  # 260714 - series 13
+  _Region(experiment='260714', series='13', name='medium', description='Bottom of the environment in the medium', index=1, vertical_sort_index=4),
+  _Region(experiment='260714', series='13', name='barrier-edge', description='Within one cell diameter of the barrier', index=2, vertical_sort_index=3),
+  _Region(experiment='260714', series='13', name='barrier', description='Within the barrier', index=3, vertical_sort_index=2),
+  _Region(experiment='260714', series='13', name='gel', description='Through the barrier in the gel region', index=4, vertical_sort_index=1),
+
+  # 260714 - series 14
+  _Region(experiment='260714', series='14', name='medium', description='Bottom of the environment in the medium', index=1, vertical_sort_index=4),
+  _Region(experiment='260714', series='14', name='barrier-edge', description='Within one cell diameter of the barrier', index=2, vertical_sort_index=3),
+  _Region(experiment='260714', series='14', name='barrier', description='Within the barrier', index=3, vertical_sort_index=2),
+  _Region(experiment='260714', series='14', name='gel', description='Through the barrier in the gel region', index=4, vertical_sort_index=1),
+
+  # 260714 - series 15
+  _Region(experiment='260714', series='15', name='medium', description='Bottom of the environment in the medium', index=1, vertical_sort_index=4),
+  _Region(experiment='260714', series='15', name='barrier-edge', description='Within one cell diameter of the barrier', index=2, vertical_sort_index=3),
+  _Region(experiment='260714', series='15', name='barrier', description='Within the barrier', index=3, vertical_sort_index=2),
+  _Region(experiment='260714', series='15', name='gel', description='Through the barrier in the gel region', index=4, vertical_sort_index=1),
+
+  # 280614 - series 7
+  _Region(experiment='280614', series='7', name='medium', description='Bottom of the environment in the medium', index=1, vertical_sort_index=4),
+  _Region(experiment='280614', series='7', name='barrier-edge', description='Within one cell diameter of the barrier', index=2, vertical_sort_index=3),
+  _Region(experiment='280614', series='7', name='barrier', description='Within the barrier', index=3, vertical_sort_index=2),
+  _Region(experiment='280614', series='7', name='gel', description='Through the barrier in the gel region', index=4, vertical_sort_index=1),
+
 )
 
 ### Image file extensions
