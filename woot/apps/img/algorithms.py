f7fb82222a4b63b98a956ff0d24731df773a9cc1
NicholasPiano <nicholas.d.piano@gmail.com>
2015-05-10 13:42:45 +0100

    debug

--- woot/apps/img/algorithms.py
+++ woot/apps/img/algorithms.py
@@ -205,6 +205,7 @@ def mod_step09_regions(composite, mod_id, algorithm):
 
   # iterate
   for t in range(composite.series.ts):
+    print(t)
     region_img = region_img_set.get(t=t)
 
     # for each image, determine unique values of labelled array
