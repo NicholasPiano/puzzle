4f4f2f7340c1b4f41ba4d308b5f7d808d1cae5c6
NicholasPiano <nicholas.d.piano@gmail.com>
2015-05-20 01:48:40 +0100

    run only 260714_s13

--- run.sh
+++ run.sh
@@ -1,41 +1,7 @@
-time python manage.py step01_input --expt=050714 --series=13;
-time python manage.py step01_input --expt=260714 --series=12;
 time python manage.py step01_input --expt=260714 --series=13;
-time python manage.py step01_input --expt=260714 --series=14;
-time python manage.py step01_input --expt=260714 --series=15;
-
-time python manage.py step03_pmod --expt=050714 --series=13;
-time python manage.py step03_pmod --expt=260714 --series=12;
 time python manage.py step03_pmod --expt=260714 --series=13;
-time python manage.py step03_pmod --expt=260714 --series=14;
-time python manage.py step03_pmod --expt=260714 --series=15;
-
-time python manage.py step07_tracks --expt=050714 --series=13;
-time python manage.py step07_tracks --expt=260714 --series=12;
 time python manage.py step07_tracks --expt=260714 --series=13;
-time python manage.py step07_tracks --expt=260714 --series=14;
-time python manage.py step07_tracks --expt=260714 --series=15;
-
-time python manage.py step08_reduced --expt=050714 --series=13;
-time python manage.py step08_reduced --expt=260714 --series=12;
 time python manage.py step08_reduced --expt=260714 --series=13;
-time python manage.py step08_reduced --expt=260714 --series=14;
-time python manage.py step08_reduced --expt=260714 --series=15;
-
-time python manage.py step09_regions --expt=050714 --series=13;
-time python manage.py step09_regions --expt=260714 --series=12;
 time python manage.py step09_regions --expt=260714 --series=13;
-time python manage.py step09_regions --expt=260714 --series=14;
-time python manage.py step09_regions --expt=260714 --series=15;
-
-time python manage.py step10_cp --expt=050714 --series=13;
-time python manage.py step10_cp --expt=260714 --series=12;
 time python manage.py step10_cp --expt=260714 --series=13;
-time python manage.py step10_cp --expt=260714 --series=14;
-time python manage.py step10_cp --expt=260714 --series=15;
-
-time python manage.py step11_masks --expt=050714 --series=13;
-time python manage.py step11_masks --expt=260714 --series=12;
 time python manage.py step11_masks --expt=260714 --series=13;
-time python manage.py step11_masks --expt=260714 --series=14;
-time python manage.py step11_masks --expt=260714 --series=15;
