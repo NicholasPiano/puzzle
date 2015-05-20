c2915d9cbcf85ba0bd1319b4deff59cfdaa26f5d
NicholasPiano <nicholas.d.piano@gmail.com>
2015-05-20 01:18:35 +0100

    adding bash script

--- woot/apps/expt/management/commands/step13_cells.py
+++ woot/apps/expt/management/commands/step13_cells.py
@@ -7,13 +7,14 @@ from django.core.management.base import BaseCommand, CommandError
 from apps.expt.models import Series
 from apps.expt.util import *
 from apps.expt.data import *
-from apps.img.util import nonzero_mean
+from apps.img.util import nonzero_mean, cut_to_black
 
 # util
 import os
 import numpy as np
 from optparse import make_option
 import matplotlib.pyplot as plt
+from scipy.misc import imsave
 from scipy.ndimage.measurements import center_of_mass as cm
 from scipy.ndimage.morphology import distance_transform_edt as dt
 from scipy.ndimage.morphology import binary_erosion as be
@@ -64,4 +65,33 @@ class Command(BaseCommand):
     series = Series.objects.get(experiment__name=options['expt'], name=options['series'])
     composite = series.composites.get()
 
-    
+    for t in range(1):
+      # load entire set of mask gons as 3D box with accessor dictionary
+      mask_gon_stack = None
+      mask_accessor_dict = {}
+
+      mask_gon_set = composite.gons.filter(channel__name='bfreduced', t=t)
+
+      for i, mask_gon in enumerate(mask_gon_set):
+        m = mask_gon.load()
+        mask_accessor_dict[mask_gon.pk] = i
+        if mask_gon_stack is None:
+          mask_gon_stack = m
+        else:
+          mask_gon_stack = np.dstack([mask_gon_stack, m])
+
+      def slice(z=None, pk=None):
+        if z is None:
+          return mask_gon_stack[:,:,mask_accessor_dict[pk]]
+        else:
+          return mask_gon_stack[:,:,mask_accessor_dict[mask_gon_set.get(z=z).pk]]
+
+      # to access a single mask in the bulk, need (gon.pk or gon.z, unique_id)
+      for z in range(series.zs):
+        # full_mask = np.zeros(series.shape(), dtype=int)
+        mask_set = list(composite.masks.filter(max_z=z, gon__t=t, gon__channel__name='bfreduced'))
+        for mask in mask_set:
+          print(mask.pk)
+          # full_mask += (slice(pk=mask.gon.pk)==mask.mask_id).astype(int)
+
+        # imsave(os.path.join(series.experiment.output_path, 'level_{}.tiff'.format(z)), full_mask)
--- woot/apps/expt/models.py
+++ woot/apps/expt/models.py
@@ -235,6 +235,9 @@ class Series(models.Model):
     region = list(filter(lambda x: x.experiment==self.experiment.name and x.series==self.name and x.index==index, regions))[0]
     return region.vertical_sort_index
 
+  def shape(self):
+    return (self.rs, self.cs)
+
 class Channel(models.Model):
   # connections
   experiment = models.ForeignKey(Experiment, related_name='channels')
