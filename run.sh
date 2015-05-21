d1badb3762e0233a676e57f0beff75501f2b7c94
NicholasPiano <nicholas.d.piano@gmail.com>
2015-05-21 12:19:51 +0100

    not using cellprofiler

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 time python manage.py step01_input --expt=260714 --series=13;
-time python manage.py step03_pmod --expt=260714 --series=13;
 time python manage.py step07_tracks --expt=260714 --series=13;
-time python manage.py step08_reduced --expt=260714 --series=13;
 time python manage.py step09_regions --expt=260714 --series=13;
