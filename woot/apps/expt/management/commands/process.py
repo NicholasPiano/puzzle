f3a03c3e581a1d6536a9d258df9708004565e21c
NicholasPiano <nicholas.d.piano@gmail.com>
2015-05-24 12:16:01 +0100

    cleaning up commands

--- woot/apps/expt/management/commands/process.py
+++
@@ -1,8 +0,0 @@
-# scaling
-# one cell moving, different colour for each region
-# displacement from barrier x
-#
-
-# 1. velocity squared in each region
-# 2. total displacement in -x
-# 3. total displacement squared.
