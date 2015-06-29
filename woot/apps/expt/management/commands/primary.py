0a9368ac6b77d652e60cd0623d55c2a774250cf5
NicholasPiano <nicholas.d.piano@gmail.com>
2015-06-29 15:41:18 +0100

    primary

---
+++ woot/apps/expt/management/commands/primary.py
@@ -0,0 +1,92 @@
+# expt.command: step11_reduced
+
+# django
+from django.core.management.base import BaseCommand, CommandError
+
+# local
+from apps.expt.models import Series
+
+# util
+import os
+import numpy as np
+from optparse import make_option
+import matplotlib.pyplot as plt
+from scipy.ndimage.filters import gaussian_filter as gf
+from skimage import exposure
+from skimage import filter as ft
+from scipy.misc import imsave, imread
+from scipy.optimize import curve_fit
+
+def scan_point(img, rs, cs, r, c, size=0):
+  r0 = r - size if r - size >= 0 else 0
+  r1 = r + size + 1 if r + size + 1 <= rs else rs
+  c0 = c - size if c - size >= 0 else 0
+  c1 = c + size + 1 if c + size + 1 <= cs else cs
+
+  column = img[r0:r1,c0:c1,:]
+  column_1D = np.sum(np.sum(column, axis=0), axis=0)
+
+  return column_1D
+
+### Command
+class Command(BaseCommand):
+  option_list = BaseCommand.option_list + (
+
+    make_option('--expt', # option that will appear in cmd
+      action='store', # no idea
+      dest='expt', # refer to this in options variable
+      default='050714', # some default
+      help='Name of the experiment to import' # who cares
+    ),
+
+    make_option('--series', # option that will appear in cmd
+      action='store', # no idea
+      dest='series', # refer to this in options variable
+      default='13', # some default
+      help='Name of the series' # who cares
+    ),
+
+  )
+
+  args = ''
+  help = ''
+
+  def handle(self, *args, **options):
+    '''
+    1. What does this script do?
+    > Use masks to build up larger masks surrounding markers
+
+    2. What data structures are input?
+    > Mask, Gon
+
+    3. What data structures are output?
+    > Channel, Gon, Mask
+
+    4. Is this stage repeated/one-time?
+    > Repeated
+
+    Steps:
+
+    1. load mask gons
+    2. stack vertically in single array
+
+    '''
+
+    # vars
+    series = Series.objects.get(experiment__name=options['expt'], name=options['series'])
+    composite = series.composites.get()
+
+    # get all markers
+    for t in range(series.ts):
+
+      markers = series.markers.filter(t=t)
+
+      black = np.zeros(series.shape(d=2))
+
+      for marker in markers:
+        r0, r1 = marker.r - 3, marker.r + 2
+        c0, c1 = marker.c - 3, marker.c + 2
+
+        black[r0:r1,c0:c1] = 255
+
+      imsave(os.path.join(series.experiment.composite_path, 'primary_t{}.tiff'.format(t)))
